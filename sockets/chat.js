2ad087963b29e75a1676e9ab99159184a24a64d1
HolyPi <joanelly2018@gmail.com>
2021-05-04 20:51:06 -0400

    Users can join and leave chats

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,16 +1,33 @@
-//chat.js
-module.exports = (io, socket) => {
+module.exports = (io, socket, onlineUsers) => {
 
     socket.on('new user', (username) => {
+      //Save the username as key to access the user's socket id
+      onlineUsers[username] = socket.id;
+      //Save the username to socket as well. This is important for later.
+      socket["username"] = username;
       console.log(`✋ ${username} has joined the chat! ✋`);
       io.emit("new user", username);
     })
   
-    //Listen for new messages
     socket.on('new message', (data) => {
-      // Send that data back to ALL clients
       console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
       io.emit('new message', data);
     })
+
+    socket.on('get online users', () => {
+    //Send over the onlineUsers
+        socket.emit('get online users', onlineUsers);
+    })
+    
+    // This fires when a user closes out of the application
+    // socket.on("disconnect") is a special listener that fires when a user exits out of the application.
+    socket.on('disconnect', () => {
+      console.log(`✋ ${socket.username} has left the chat! ✋`);
+      //This deletes the user by using the username we saved to the socket
+      delete onlineUsers[socket.username]
+      io.emit('user has left', onlineUsers);
+    });
+
+
   
-  }
+}
